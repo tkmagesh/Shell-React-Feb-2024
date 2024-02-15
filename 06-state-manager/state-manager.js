0784ff41de526b97a21062d5e30216b16623a256
Magesh Kuppan <tkmagesh77@gmail.com>
2024-02-15 14:18:17 +0530

    Handling 'payloads'

--- 06-state-manager/state-manager.js
+++ 06-state-manager/state-manager.js
@@ -34,6 +34,17 @@ let StateManager = (function(){
         return store;
     }
 
-    return { createStore }
+    function bindActionCreators(actionCreators, dispatch){
+        const actionDispatchers = {}
+        for (let key in actionCreators){
+            actionDispatchers[key] = function(...args){
+                const action = actionCreators[key](...args)
+                dispatch(action)
+            }
+        }
+        return actionDispatchers;
+    }
+
+    return { createStore, bindActionCreators }
 
 })()
